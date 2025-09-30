5f83776d532e33dd8537941f1a439c5cff9ec87b
Nahum <flashjzd58@gmail.com>
2025-09-30 16:58:45 -0600

    cambios pdfs

--- app/Models/Correos.php
+++ app/Models/Correos.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Correos extends Model
 {
     use HasFactory;
-    protected $table = 'correos';
+    protected $table = 'Correos';
 //    public $timestamps = false;
     protected $guarded = ["id"];
 }
